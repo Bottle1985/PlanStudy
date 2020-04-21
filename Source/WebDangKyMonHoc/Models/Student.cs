3e297f178d3bfc2633ac6b49c267bd75c399744e
Bottle1985 <mrboongvn@yahoo.com.vn>
2020-04-21 08:33:45 +0700

    Added class student

---
+++ Source/WebDangKyMonHoc/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDangKyMonHoc.Models
+{
+    public class Student
+    {
+        private String rollNo;
+        private String name;
+
+        public String getRollNo()
+        {
+            return rollNo;
+        }
+
+        public void setRollNo(String rollNo)
+        {
+            this.rollNo = rollNo;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+    }
+}
