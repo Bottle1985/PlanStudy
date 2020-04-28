3477b312a50703c70029c66a8b85a46a65b255f8
Bottle1985 <mrboongvn@yahoo.com.vn>
2020-04-28 08:36:47 +0700

    + Added template connect database function

---
+++ Source/WebDangKyMonHoc/Models/Entities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDangKyMonHoc.Models
+{
+    public class Entities
+    {
+        public Entities() : base("demoASPEntities")
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+
+
+        }
+
+    }
+}
